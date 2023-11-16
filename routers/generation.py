d87b18fff65ac2c535ca6fb8cb90812f5d1a0bf9
kim2ju <bamtori05@naver.com>
2023-11-17 01:47:14 +0900

    feat: 문제 생성 베이스 코드 추가

--- routers/generation.py
+++ routers/generation.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, UploadFile
 from pydantic import BaseModel
+from dotenv import load_dotenv
 import PyPDF2
+from openai import OpenAI
+
+load_dotenv()
 
 router = APIRouter(
     prefix="/generation",
 )
-
 
 class Text(BaseModel):
     text: str
@@ -22,3 +25,18 @@ def upload_lecture_file(file: UploadFile):
         text += '\n'
 
     return text
+
+
+@router.post('/question', tags=['generation'])
+def make_question(text: Text):
+    client = OpenAI()
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "다음 내용 바탕으로 TF 3문제를 답과 함께 생성해주세요."},
+            {"role": "user", "content": text.text}
+        ]
+    )
+
+    return response.choices[0].message.content
